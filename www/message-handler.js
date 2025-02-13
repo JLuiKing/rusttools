59a5332d1802a6bf04b773e6c5024fc5275c0fb7
J.Lui-King <163066317+JLuiKing@users.noreply.github.com>
2025-02-14 10:00:21 +1100

    Add files via upload

---
+++ www/message-handler.js
@@ -0,0 +1,8 @@
+// This receives messages of type "testmessage" from the server.
+// See http://shiny.rstudio.com/gallery/server-to-client-custom-messages.html
+// for details
+Shiny.addCustomMessageHandler("testmessage",
+  function(message) {
+    alert(JSON.stringify(message));
+  }
+);
